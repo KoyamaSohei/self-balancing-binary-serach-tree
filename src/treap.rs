f0254d743d27e212651a7df9ad3d3a690d05a49c
KoyamaSohei <koyamaso0309@gmail.com>
2021-03-21 02:16:59 +0900

    improve test

--- src/treap.rs
+++ src/treap.rs
@@ -1,6 +1,8 @@
 use std::fmt::Debug;
 use std::mem::{replace, swap};
 
+use rand::Rng;
+
 use super::BBST;
 
 // https://faculty.washington.edu/aragon/pubs/rst96.pdf
@@ -15,11 +17,17 @@ struct Node<K: Ord> {
 
 pub struct Treap<K: Ord + Debug> {
   root: Option<Box<Node<K>>>,
+  rng: rand::rngs::StdRng,
 }
 
 impl<K: Ord + Debug> Treap<K> {
   pub fn new() -> Treap<K> {
-    Treap { root: None }
+    Treap {
+      root: None,
+      rng: rand::SeedableRng::from_seed([77; 32]),
+    }
+  }
+
   }
 
   fn _insert(item: Box<Node<K>>, tree: &mut Option<Box<Node<K>>>) {
@@ -36,13 +44,36 @@ impl<K: Ord + Debug> Treap<K> {
       }
     }
   }
+  #[cfg(test)]
+  fn _print(tree: &Box<Node<K>>) -> String {
+    let mut message = String::from("[");
+    message = [message, format!("{:?}(", tree.key)].concat();
+    match &tree.lchild {
+      None => {}
+      Some(l) => {
+        message = [message, Treap::_print(&l)].concat();
+      }
+    }
+    message = [message, String::from(")(")].concat();
+    match &tree.rchild {
+      None => {}
+      Some(r) => {
+        message = [message, Treap::_print(&r)].concat();
+      }
+    }
+    [message, String::from(")]")].concat()
+  }
+  #[cfg(test)]
+  pub fn print(&self) -> String {
+    Treap::_print(self.root.as_ref().unwrap())
+  }
 }
 
 impl<K: Ord + Debug> BBST<K> for Treap<K> {
   fn insert(&mut self, key: K) {
     let item: Box<Node<K>> = Box::new(Node {
       key,
-      priority: rand::random(),
+      priority: self.rng.gen(),
       lchild: None,
       rchild: None,
     });
@@ -54,38 +85,32 @@ impl<K: Ord + Debug> BBST<K> for Treap<K> {
 mod tests {
   use super::Treap;
   use super::BBST;
+  use assert_str::assert_str_eq;
 
   #[test]
   fn test_insert() {
     let mut tree: Treap<u64> = Treap::new();
     tree.insert(10 as u64);
-    assert_eq!(tree.root.as_deref().unwrap().key, 10 as u64);
+    assert_str_eq!(tree.print(), "[10()()]");
   }
   #[test]
   fn test_insert_2() {
     let mut tree: Treap<u64> = Treap::new();
     tree.insert(10 as u64);
-    assert_eq!(tree.root.as_deref().unwrap().key, 10 as u64);
+    assert_str_eq!(tree.print(), "[10()()]");
     tree.insert(50 as u64);
-    {
-      let r = tree.root.as_deref().unwrap().rchild.as_ref();
-      assert_eq!(r.as_deref().unwrap().key, 50 as u64);
-    }
+    assert_str_eq!(tree.print(), "[10()([50()()])]");
   }
   #[test]
   fn test_insert_3() {
     let mut tree: Treap<u64> = Treap::new();
     tree.insert(10 as u64);
-    assert_eq!(tree.root.as_deref().unwrap().key, 10 as u64);
+    assert_str_eq!(tree.print(), "[10()()]");
     tree.insert(50 as u64);
-    {
-      let r = tree.root.as_deref().unwrap().rchild.as_ref();
-      assert_eq!(r.as_deref().unwrap().key, 50 as u64);
-    }
+    assert_str_eq!(tree.print(), "[10()([50()()])]");
     tree.insert(5 as u64);
-    {
-      let l = tree.root.as_deref().unwrap().lchild.as_ref();
-      assert_eq!(l.as_deref().unwrap().key, 5 as u64);
-    }
+    assert_str_eq!(tree.print(), "[10([5()()])([50()()])]");
+  }
+
   }
 }
